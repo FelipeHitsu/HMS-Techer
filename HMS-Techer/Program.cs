5e5c531cc2b680c3dc89bc2a6079eda4944b3f3b
Felipe <felipe.silva.hts@gmail.com>
2021-02-24 16:49:14 -0300

    Init Repositorie

---
+++ HMS-Techer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HMS_Techer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
